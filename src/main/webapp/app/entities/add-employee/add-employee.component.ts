8bb0e4f579a7ce0b66ea949e3a1aec71aa744fd9
srs <srs@srs-ThinkPad-T470>
2019-12-19 11:24:49 +0530

    ui changes

--- src/main/webapp/app/entities/add-employee/add-employee.component.ts
+++ src/main/webapp/app/entities/add-employee/add-employee.component.ts
@@ -2,7 +2,6 @@ import { IAddEmployee, AddEmployee } from './add-employee.model';
 import { AssetInventoryService } from './../assetinventory.service';
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-import { FormBuilder,Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'jhi-add-employee',
@@ -11,14 +10,10 @@ import { FormBuilder,Validators, FormGroup } from '@angular/forms';
 })
 export class AddEmployeeComponent implements OnInit {
   addEmployee: IAddEmployee;
-  registerForm: FormGroup;
-  constructor(private service: AssetInventoryService,private formBuilder: FormBuilder){}
+  constructor(private service: AssetInventoryService) {}
 
   ngOnInit() {
     this.addEmployee = new AddEmployee();
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]]    
-  });
   }
   onSave() {
     this.service.createEmployee(this.addEmployee).subscribe(res => {
